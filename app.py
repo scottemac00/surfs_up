63d1d7227684732b4ea202b325324c867baf07e0
scottemac00 <scott.e.mccraney@gmail.com>
2022-11-29 21:40:14 -0500

    having to commit and push to run app?

--- app.py
+++ app.py
@@ -32,11 +32,21 @@ app = Flask(__name__)
 def welcome():
     return(
     '''
-    Welcome to the Climate Analysis API!
-    Available Routes:
-    /api/v1.0/precipitation
-    /api/v1.0/stations
-    /api/v1.0/tobs
-    /api/v1.0/temp/start/end    
+    Welcome to the Climate Analysis API!.\
+    Available Routes:.\
+    /api/v1.0/precipitation.\
+    /api/v1.0/stations.\
+    /api/v1.0/tobs.\
+    /api/v1.0/temp/start/end
     ''')
+
+# define a route for the precipitation function
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}    
+    return jsonify(precip)
+
 
